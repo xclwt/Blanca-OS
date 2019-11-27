5241883a02aa4ac2ed4105788449c2887b403681
xclwt <vincent_hsu0521@qq.com>
2019-11-27 09:57:37 +0800

    添加了未完成的bootloader组件

---
+++ boot/mbr.asm
@@ -0,0 +1,96 @@
+%include "boot.inc"
+
+SECTION mbr vstart = 0x7c00
+        mov ax,cs
+        mov ss,ax
+        mov sp,0x7c00
+
+        mov eax,loader_start_sector
+        mov ebx,loader_base_addr
+        call read_hard_disk
+
+        mov eax,loader_base_addr
+        xor edx,edx
+        mov ecx,512
+        div ecx
+        
+        or edx,edx
+        jnz @1
+        dec eax
+
+    @1:
+        or eax,eax
+        jz loader_base_addr
+        
+        mov ecx,eax
+        mov eax,loader_start_sector
+        
+    @2: 
+        inc eax
+        call read_hard_disk
+        loop @2
+
+read_hard_disk:	
+        push eax
+        push ecx
+        push edx
+        
+        push eax
+        
+        mov dx,0x1f2
+        mov al,1
+        out dx,al
+        
+        inc dx
+        pop eax
+        out dx,al
+        
+        mov cl,8
+        shr eax,cl
+        inc dx
+        out dx,al
+        
+        shr eax,cl
+        inc dx
+        out dx,al
+        
+        shr eax,cl
+        and al,0x0f
+        or al,0xe0
+        inc dx
+        out dx,al
+        
+    .not_ready:
+        in al,dx
+        and al,0x88
+        cmp al,0x08
+        jnz .not_ready
+        
+        mov ecx,256
+        mov dx,0x1f0
+    .read:
+        in ax,dx
+        mov [ebx],ax
+        add ebx,2
+        loop .read
+        
+        pop edx
+        pop ecx
+        pop eax
+        
+        ret
+        
+        
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
