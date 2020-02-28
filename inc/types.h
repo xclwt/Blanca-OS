78624c72dd255db19c35427651d3f47763fee5df
xclwt <vincent_hsu0521@qq.com>
2020-02-28 23:20:52 +0800

    添加了types.h

---
+++ inc/types.h
@@ -0,0 +1,17 @@
+#ifndef _INC_TYPE_H
+#define _INC_TYPE_H
+
+#define TRUE    1
+#define FALSE   0
+#define ERROR  -1
+
+typedef char int8_t
+typedef short int16_t
+typedef int int32_t
+//typedef long int64_t
+typedef unsigned char uint8_t
+typedef unsigned short uint16_t
+typedef unsigned int uint32_t
+//typedef unsigned long uint64_t
+
+#endif
