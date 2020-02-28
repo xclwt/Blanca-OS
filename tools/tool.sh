95f2664f5906533e7c8c2e3e20e678cfd8d0d0cf
xclwt <vincent_hsu0521@qq.com>
2020-02-28 23:27:48 +0800

    更新了工具

--- tools/tool.sh
+++ tools/tool.sh
@@ -1,1 +1,1 @@
-gcc -c -m32 -o main.o main.c && ld main.o-Ttext0xc0001500 -e main -m elf_i386 -o kernel.bin && dd if=kernel.bin of=/home/xclwt/bochs/hd60M.img bs=512 count=200 seek=9 conv=notrunc
+gcc -c -m32 -o ../kernel/main.o ../kernel/main.c && ld ../kernel/main.o -Ttext 0xc0001500 -e main -m elf_i386 -o ../kernel/kernel.bin && dd if=../kernel/kernel.bin of=/home/xclwt/bochs/hd60M.img bs=512 count=200 seek=9 conv=notrunc
