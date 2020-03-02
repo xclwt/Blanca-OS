e473aa6e46c8002b44d0bdc7cb6969f55d52db7d
xclwt <vincent_hsu0521@qq.com>
2020-03-02 21:36:11 +0800

    add simplified vsprint

--- libs/printk.c
+++ libs/printk.c
@@ -1,4 +1,6 @@
-#include "../inc/types.h"
+#include <types.h>
+#include <vsprint.h>
+#include <string.h>
 
 #define is_digit(ch) (ch >= '0' && ch <= '9')
 
@@ -14,7 +16,7 @@ int atoi(const char *s){
 }
 
 char* itoa(int value, char *str, int radix){
-    char reverse[36];
+    char reverse[32];
     char *p = reverse, *q = str;
     bool positive = (value >= 0) ? TRUE : FALSE;
     value = (value >= 0) ? value : -value;
@@ -36,6 +38,66 @@ char* itoa(int value, char *str, int radix){
     return str;
 }
 
-void vsprint(char *buf, const char *fmt, va_list args){
+char* uitoa(uint32_t value, char *str, int radix){
+    char reverse[32];
+    char *p = reverse, *q = str;
     
+    *p++ = '\0';
+    
+    while(value >= 0){
+        *p++ = "0123456789abcdef"[value % radix];
+        value /= radix;
+        if(value == 0) break;
+    }
+    
+    p--;
+    
+    while(p >= reverse){
+        *q++ = *p--;
+    }
+    
+    return str;
+}
+
+void vsprint(char *buf, const char *fmt, va_list args){
+    for(;*fmt;fmt++){
+        if(*fmt != '%'){
+            *buf++ = *fmt;
+            continue;
+        }
+        
+        fmt++;
+        
+        switch(*fmt){
+            case 'd':
+                itoa(va_arg(args, int), buf, 10);
+                buf += strlen(buf);
+                break;
+            case 'x':
+                uitoa(va_arg(args, unsigned int), buf, 16);
+                buf += strlen(buf);
+                break;
+            case 'f':
+                break;
+            case 'c':
+                *buf++ = va_arg(args, char);
+                break;
+            case 's':
+                strcpy(buf, va_arg(args, char*));
+                buf += strlen(buf);
+                break;
+        }
+    }
+    *buf = 0;
+}
+
+void printk(const char * fmt, ...){
+    char buf[1024];
+    va_list args;
+    
+    va_start(args, fmt);
+    vsprint(args);
+    va_end(args);
+    
+    console_print(buf);
 }
