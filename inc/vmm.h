d68b3f84cfb8a3289936c947d950622713175087
xclwt <1260918796@qq.com>
2020-04-10 21:44:54 +0800

    update vmm

--- inc/vmm.h
+++ inc/vmm.h
@@ -3,12 +3,25 @@
 
 #include <types.h>
 #include <asm.h>
+#include <pmm.h>
+#include <string.h>
 
 #define KERNEL_BASE 0xc0000000  //内核基址
 #define KPAGE_SIZE 0x400000		//内核页表大小
 
-#define pde_index(vaddr) (vaddr >> 22) & 0x3ff
-#define pte_index(vaddr) (vaddr >> 12) & 0x3ff
+#define pde_index(vaddr) (vaddr >> 22)
+#define pte_index(vaddr) (vaddr >> 12)
+
+#define PHY_MEM_LENGTH 0x38000000
+#define PAGE_DIR_SIZE 0x400000
+#define PDE_COUNT (PHY_MEM_LENGTH / PAGE_DIR_SIZE) 
+#define PTE_COUNT (PAGE_SIZE / sizeof(uint32_t))
+
+#define PAGE_P 0x1
+#define PAGE_R 0x0
+#define PAGE_W 0x2
+#define PAGE_S 0x0
+#define PAGE_U 0x4
 
 typedef uint32_t pde_t;
 typedef uint32_t pte_t;
--- mm/vmm.c
+++ mm/vmm.c
@@ -6,7 +6,35 @@ pde_t* pgd_start = (pde_t*)0xfffff000;
 //映射到0x101000
 pte_t* pte_start = (pte_t*)0xffc00000;
 
-
 void vmm_init(){
+	uint32_t pgd_index = pde_index(KERNEL_BASE);
 	
+	for(uint32_t i = pgd_index; i < pgd_index + PDE_COUNT; ++i){
+		pgd_start[i] = ((i - pgd_index) << 22) | PAGE_P | PAGE_W | PAGE_S;
+	}
+
+	for(uint32_t i = 0; i < PDE_COUNT * PTE_COUNT; ++i){
+		pte_start[i] = (i << 12) | PAGE_P | PAGE_W | PAGE_S;
+	}
+
+	switch_pgd(KERNEL_BASE);
+}
+
+void map(pde_t* cur_pgd, uint32_t vaddr, uint32_t paddr, uint32_t flag){
+	uint32_t pgd_index = pde_index(vaddr);
+	uint32_t pte_index = pte_index(vaddr);
+
+	pte_t* pte = (pte_t*)cur_pgd[pgd_index] & PAGE_ALIGN;
+	if(!pte){
+		if(va >= KERNEL_BASE){
+			pte = (pte_t*)alloc_pages(1);
+			memset(pte, 0, PAGE_SIZE);
+			cur_pgd[pgd_index] = (uint32_t)pte | PAGE_P | flag;
+		}else{
+			pte = (pte_t*)();
+			cur_pgd[pgd_index] = (uint32_t)pte | PAGE_P | flag;
+		} 
+	}
+
+	pte[pte_index] = (pa & PAGE_ALIGN) | flags;
 }
